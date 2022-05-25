616a794944827cfbf9a96d27eb6679a97a8bfaba
Facu Mediotte <facumediotte@gmail.com>
2022-05-25 19:47:44 -0300

    First commit SB Server

---
+++ smart-farm-backend/src/main/java/api/smartfarm/controllers/FarmController.java
@@ -0,0 +1,16 @@
+package api.smartfarm.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/farms")
+public class FarmController {
+
+    @GetMapping
+    public String event() {
+        return "Hello world";
+    }
+
+}
---
+++ smart-farm-backend/src/test/java/api/smartfarm/SmartFarmBackendApplicationTests.java
@@ -0,0 +1,15 @@
+package api.smartfarm;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartFarmBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+        Assertions.assertTrue(true);
+    }
+
+}
