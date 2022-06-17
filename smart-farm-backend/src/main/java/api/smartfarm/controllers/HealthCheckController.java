dbdd8afbcf45b7099c497bf1589176f5f2e60f8a
Facu Mediotte <facumediotte@gmail.com>
2022-06-16 23:43:38 -0300

    Big Refactor of the app, create user and create farm working, healthCheck endpoint

---
+++ smart-farm-backend/src/main/java/api/smartfarm/controllers/HealthCheckController.java
@@ -0,0 +1,15 @@
+package api.smartfarm.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/health")
+public class HealthCheckController {
+
+    @GetMapping
+    public String healthCheck() {
+        return "I'm ok and healthy";
+    }
+}
