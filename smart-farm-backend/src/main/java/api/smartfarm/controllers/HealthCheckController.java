901624cb93af19fa03b9287c33d7a164d171c55c
Facu Mediotte <facumediotte@gmail.com>
2022-06-30 00:37:28 -0300

    HealthCheckController shows pom version

--- smart-farm-backend/src/main/java/api/smartfarm/controllers/HealthCheckController.java
+++ smart-farm-backend/src/main/java/api/smartfarm/controllers/HealthCheckController.java
@@ -1,5 +1,6 @@
 package api.smartfarm.controllers;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,8 +11,11 @@ import org.springframework.web.bind.annotation.RestController;
 @CrossOrigin
 public class HealthCheckController {
 
+    @Value("${app.version}")
+    private String version;
+
     @GetMapping
     public String healthCheck() {
-        return "I'm ok and healthy";
+        return "I'm ok and healthy, SmartFarm v" + version;
     }
 }
